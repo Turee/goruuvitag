04d10f3d99a81d94141cfe59a86014f3a1461b80
Turkka Mannila <turkka.mannila@vincit.fi>
2017-12-30 16:39:59 +0200

    Build script and github release job

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -rf output
+for arch in arm amd64; do
+  for os in linux; do
+    OUTDIR=output/$os/$arch
+    echo "Building $os $arch"
+    mkdir -p $OUTDIR
+    export GOOS=$os
+    export GOARCH=$arch
+    go build -o $OUTDIR/goruuvitag github.com/Turee/goruuvitag
+  done
+done
+
+echo "Creating tar file"
+tar zcvf binaries.tar.gz output
