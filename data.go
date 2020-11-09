c606819a4f06523afb07ae18910c5d2420e1f7f0
Joel Mertanen <joel.mertanen@gmail.com>
2020-11-09 18:19:05 +0200

    implement error handling, more tests, pending on cleanup

--- data.go
+++ data.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+
 	"encoding/binary"
 	"encoding/json"
 	"fmt"
@@ -45,17 +46,17 @@ type SensorFormat3 struct {
 
 //SensorData to be posted
 type SensorDataFormat5 struct {
-	Temperature         float64
-	Humidity            float64
-	Pressure            uint32
-	BatteryVoltageMv    int
-	txPower             int
-	AccelerationX       float64
-	AccelerationY       float64
-	AccelerationZ       float64
-	MovementCounter     uint8
-	MeasurementSequence uint16
-	MAC                 string
+	Temperature         *float64
+	Humidity            *float64
+	Pressure            *uint32
+	BatteryVoltageMv    *int
+	txPower             *int
+	AccelerationX       *float64
+	AccelerationY       *float64
+	AccelerationZ       *float64
+	MovementCounter     *uint8
+	MeasurementSequence *uint16
+	MAC                 *string
 }
 
 //SensorFormat5 https://github.com/ruuvi/ruuvi-sensor-protocols/blob/master/dataformat_05.md#data-format-5-protocol-specification-rawv2
@@ -119,6 +120,14 @@ func parseSensorFormat3(data []byte) *SensorData {
 	return &sensorData
 }
 
+func readTemperature(val uint16) *float64 {
+	if val == 0x8000 {
+		return nil
+	}
+	result := float64(fromTwosComplement(val, 16)) / 200
+	return &result
+}
+
 func fromTwosComplement(bytes uint16, bits uint16) int {
 	mask := uint16(math.Pow(2, float64(bits-1)))
 	result := int(bytes & ^mask)
@@ -127,25 +136,92 @@ func fromTwosComplement(bytes uint16, bits uint16) int {
 	return signBit + result
 }
 
-func readPowerInfo(powerInfo uint16) (int, int) {
+func readPowerInfo(powerInfo uint16) (*int, *int) {
 	// Read only first 11 bits out of 16 for battery voltage
 	// Make a copy of it first because we need powerInfo later
-	batteryVoltage := int(powerInfo)>>5 + 1600
+	batteryVoltage := new(int)
+	*batteryVoltage = int(powerInfo) >> 5
+	if *batteryVoltage == 2047 {
+		batteryVoltage = nil
+	} else {
+		*batteryVoltage += 1600
+	}
 
+	txPower := new(int)
 	// Read last five bits for TX Power in increments of two
-	txPower := (int(powerInfo)&0b11111)*2 - 40
+	*txPower = (int(powerInfo) & 0b11111)
 
-	return batteryVoltage, int(txPower)
+	if *txPower == 31 {
+		txPower = nil
+	} else {
+		*txPower = *txPower*2 - 40
+	}
+
+	return batteryVoltage, txPower
 }
 
-func readMAC(data []byte) string {
+func readMAC(data []byte) *string {
+	isValid := false
+	for i := 0; i < len(data); i++ {
+		if data[i] != 0xFF {
+			isValid = true
+			break
+		}
+	}
+	if !isValid {
+		return nil
+	}
+
 	result := make([]string, len(data))
 
 	for i := 0; i < len(data); i++ {
 		result[i] = fmt.Sprintf("%X", data[i])
 	}
 
-	return strings.Join(result, ":")
+	val := strings.Join(result, ":")
+	return &val
+}
+
+func getAcceleration(val uint16) *float64 {
+	if val == 0x8000 {
+		return nil
+	}
+	result := float64(fromTwosComplement(val, 16)) / 1000
+	return &result
+}
+
+func readHumidity(val uint16) *float64 {
+	if val == 0xFFFF {
+		return nil
+	}
+	result := float64(val) / 400
+	return &result
+}
+
+func readPressure(val uint16) *uint32 {
+	if val == 0xFFFF {
+		return nil
+	}
+	result := uint32(val) + 50000
+	return &result
+}
+
+func readMeasurementSequence(val uint16) *uint16 {
+	if val == 0xFFFF {
+		return nil
+	}
+	return &val
+}
+
+func readMovementCounter(val uint8) *uint8 {
+	if val == 0xFF {
+		return nil
+	}
+	return &val
+}
+
+func bToUint(first byte, second byte) uint16 {
+	return uint16(first)<<8 + uint16(second)
 }
 
 // https://github.com/ruuvi/ruuvi-sensor-protocols
@@ -158,18 +234,19 @@ func parseSensorFormat5(data []byte) *SensorDataFormat5 {
 	}
 	fmt.Printf("%+v\n", result)
 	sensorData := SensorDataFormat5{}
-	sensorData.Temperature = float64(fromTwosComplement(result.Temperature, 16)) / 200
-	sensorData.Humidity = float64(result.Humidity) / 400
-	sensorData.Pressure = uint32(result.Pressure) + 50000
+	sensorData.Temperature = readTemperature(bToUint(data[1], data[2]))
+
+	sensorData.Humidity = readHumidity(bToUint(data[3], data[4]))
+	sensorData.Pressure = readPressure(bToUint(data[5], data[6]))
 
 	batteryVoltage, txPower := readPowerInfo(result.PowerInfo)
 	sensorData.BatteryVoltageMv = batteryVoltage
 	sensorData.txPower = txPower
-	sensorData.MeasurementSequence = result.MeasurementSequence
-	sensorData.MovementCounter = result.MovementCounter
-	sensorData.AccelerationX = float64(fromTwosComplement(result.AccelerationX, 16)) / 1000
-	sensorData.AccelerationY = float64(fromTwosComplement(result.AccelerationY, 16)) / 1000
-	sensorData.AccelerationZ = float64(fromTwosComplement(result.AccelerationZ, 16)) / 1000
+	sensorData.MeasurementSequence = readMeasurementSequence(bToUint(data[16], data[17]))
+	sensorData.MovementCounter = readMovementCounter(uint8(data[15]))
+	sensorData.AccelerationX = getAcceleration(bToUint(data[7], data[8]))
+	sensorData.AccelerationY = getAcceleration(bToUint(data[9], data[10]))
+	sensorData.AccelerationZ = getAcceleration(bToUint(data[11], data[12]))
 
 	sensorData.MAC = readMAC(data[18:24])
 	fmt.Printf("%+v\n", sensorData)
