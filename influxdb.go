af0893b2c5864837bca0fd10a113c4c1e497c395
Joel Mertanen <joel.mertanen@gmail.com>
2020-11-12 17:06:40 +0200

    use viper for settings, allow labeling

--- influxdb.go
+++ influxdb.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"os"
+	"strings"
 	"time"
 
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 	"github.com/influxdata/influxdb-client-go/v2/api"
+	"github.com/spf13/viper"
 )
 
 var client influxdb2.Client
@@ -13,22 +14,35 @@ var writeAPI api.WriteAPI
 
 // InitializeClient is required to open the connection
 func InitializeClient() {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	if err := viper.ReadInConfig(); err != nil {
+		// no point to continue without a config
+		panic(err)
+	}
+
 	// Create a new client using an InfluxDB server base URL and an authentication token
 	// and set batch size to 20
-	client = influxdb2.NewClientWithOptions(os.Getenv("INFLUXDB_HOST"), os.Getenv("INFLUXDB_TOKEN"),
+	client = influxdb2.NewClientWithOptions(viper.GetString("influxdb.host"), viper.GetString("influxdb.token"),
 		influxdb2.DefaultOptions().SetBatchSize(20))
 	// Get non-blocking write client
-	writeAPI = client.WriteAPI(os.Getenv("INFLUXDB_ORG"), os.Getenv("INFLUXDB_BUCKET"))
+	writeAPI = client.WriteAPI(viper.GetString("influxdb.org"), viper.GetString("influxdb.bucket"))
 }
 
 // WriteData writes a single point to InfluxDB. Because the client is batched, the writes may not happen
 // immediately. Currently no error handling if the client dies for some reason or so. :)
 func WriteData(sensorData *SensorData) {
+	labels := viper.GetStringMapString("ruuvitag-labels")
+	// everything is lower cased for viper configs
+	label, exists := labels[strings.ToLower(*sensorData.MAC)]
+	if !exists {
+		label = *sensorData.MAC
+	}
 	// create point
 	p := influxdb2.NewPoint(
 		"system",
 		map[string]string{
-			"address": *sensorData.MAC,
+			"label": label,
 		},
 		map[string]interface{}{
 			"temperature":         *sensorData.Temperature,
@@ -41,6 +55,7 @@ func WriteData(sensorData *SensorData) {
 			"movementCounter":     *sensorData.MovementCounter,
 			"measurementSequence": *sensorData.MeasurementSequence,
 			"acceleration":        *sensorData.Acceleration,
+			"address":             *sensorData.MAC,
 		},
 		time.Now())
 	// write asynchronously
